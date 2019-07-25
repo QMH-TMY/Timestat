bdf77eee14e3fc1466b6c1f04a955a0b143ef98f
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-25 12:16:24 +0800

    modify file

--- Timestat.py
+++ Timestat.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Copyright 2019 Shieber
-# All Rights Reserved.
 #
+#    Author: Shieber
+#
+#                             APACHE LICENSE
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+#                            Function Description
 #    1.通过邮件统计每天花费的时间
 #    2.表保存的格式为xlsx         
 #    3.后期将加入每月总花费时间统计的功能      
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 
 import poplib 
